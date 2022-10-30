ba82b01084cefcda96ef80ba1a81757c14a12f91
Konstantin Moskvin <mr.koctia@gmail.com>
2022-10-30 17:22:00 +0300

    refactor: added alternative content for multimedia according to rules 1.2 html-and-css-extended.md

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="button delete";
   deleteButtonImg.src='./remove.svg';
   deleteButtonImg.className="remove-img";
+  deleteButtonImg.alt="the picture shows a sign remove";
   deleteButton.appendChild(deleteButtonImg);
 
 
